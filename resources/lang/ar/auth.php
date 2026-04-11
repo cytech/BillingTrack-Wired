917ce442e68eaf5c68edad829408ad0ff553fd03
Mohammad <sop7ana.allah@gmail.com>
2026-04-11 20:47:57 +0200

    Add Arabic language support and some dark theme fixes

---
+++ resources/lang/ar/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'بيانات الاعتماد هذه غير متطابقة مع السجلات لدينا.',
+    'password' => 'كلمة المرور المقدمة غير صحيحة.',
+    'throttle' => 'محاولات تسجيل دخول كثيرة جداً. يرجى المحاولة مرة أخرى بعد :seconds ثانية.',
+
+];
